5cc09852c7399a89ac8505518d0f26834ab7e38e
thuanhd2 <hdthuan.1990@gmail.com>
2025-08-08 17:51:17 +0700

    Fake CLI allow seed code file

--- fake-cli/fake.sh
+++ fake-cli/fake.sh
@@ -17,6 +17,9 @@ echo ""
 # in ra input mỗi 2 giây, tối đa 5 lần
 for i in {1..5}; do
     echo "$(date '+%H:%M:%S') - Input: $input"
+    # create file dummy_code.txt with random content
+    echo "This is dummy code ${i}" > dummy_code_${i}.txt
+    echo "Created file dummy_code_${i}.txt with random content"
     sleep 2
 done
 
