71cd164f5e7b77e99c2d851f1bc5b9be1c9de508
thuanhd2 <hdthuan.1990@gmail.com>
2025-08-08 12:34:16 +0700

    Remove database script

---
+++ scripts/remove-vk-dbs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# check all databases that has name starts with auto_devs_vk and remove them all
+PREFFIX="auto_devs_vk"
+db_list=$(psql -d postgres -c "SELECT datname FROM pg_database WHERE datname LIKE '$PREFFIX%'")
+for db in $db_list; do
+    echo "Removing database: $db"
+    psql -d postgres -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '$db' AND pid <> pg_backend_pid();" && dropdb $db
+done
