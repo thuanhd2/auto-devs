22df2a9bfa36b0e9f8d7ff1cb939763505d2336b
thuanhd2 <hdthuan.1990@gmail.com>
2025-08-11 15:43:41 +0700

    Remove unused file

--- frontend/src/components/pr/index.ts
+++
@@ -1,6 +0,0 @@
-export { PRList } from './pr-list'
-export { PRDetail } from './pr-detail'
-export { PRActions } from './pr-actions'
-export { PRStatusBadge } from './pr-status-badge'
-export { PRIntegration } from './pr-integration'
-export { default as PRManagementPage } from './pr-management-page'
--- frontend/src/pages/projects/index.ts
+++
@@ -1,3 +0,0 @@
-export { ProjectList } from './ProjectList'
-export { ProjectDetail } from './ProjectDetail'
-export { EditProject } from './EditProject'
